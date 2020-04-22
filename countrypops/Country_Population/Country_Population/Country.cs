dc63e28f90df7f1fbd78b6f526f2ab1466e8a227
pasanprem <46164432+pasanprem@users.noreply.github.com>
2020-04-22 22:25:29 +0100

    Initial commmit

---
+++ countrypops/Country_Population/Country_Population/Country.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Country_Population
+{
+    internal class Country
+    {
+        public Country(string name, string code, string region, int population)
+        {
+            Name = name;
+            Code = code;
+            Region = region;
+            Population = population;
+        }
+
+        public string Name { get; }
+        public string Code { get; }
+        public string Region { get; }
+        public int Population { get; }
+
+
+    }
+}
---
+++ countrypops/Country_Population/Country_Population/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Country_Population
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = @"C:\Users\pasan.premaratne\Google Drive\_coding_\cs-collections\countrypops\worldbank_pop_data.csv";
+            CsvReader reader = new CsvReader(filepath);
+
+            List<Country> countries = reader.ReadAllCountries();
+
+            foreach (Country country in countries)
+            {
+                Console.WriteLine($"Country is {country.Name} and the population is {country.Population}");
+            }
+        }
+    }
+}
